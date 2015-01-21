39add36d368cafabe7d953ce827fe6ff24783286
Ben Auchter <bauchter@ecw.org>
2015-01-21 17:08:26 -0600

    Added a reverse tunnel ssh command to the startup service

--- field_code/LoggerMainInit.sh
+++ field_code/LoggerMainInit.sh
@@ -1,1 +1,14 @@
-/usr/bin/nohup /usr/bin/python /srv/field-research/field-code/LoggerMain.py >&/dev/null
+#!/bin/sh
+#
+# This script runs at boot-up if the service is enabled 
+# First it launches the Logger program.
+#
+# After that, it tries to read one file which specifies the reverse tunnel port
+# for this remote field device.  
+# ASSUMES FILE EXISTS.  
+# ASSUMES FILE HAS A VALID PORT THAT HASN'T BEEN ALLOCATED ALREADY
+# 
+# ASSUMES SSH keys have been generated and shared with rsync destination!
+/usr/bin/nohup /usr/bin/python /srv/field-research/field-code/LoggerMain.py >&/dev/null &
+RPORT=$(cat /srv/field-research/field-code/reverseSSHport)
+/usr/bin/ssh -fN -R $RPORT:localhost:22 frsa@app6.ecw.org >&/dev/null &
